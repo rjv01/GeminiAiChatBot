4ba0b4dbe588be3aefca09989fcd2622c73f71e2
rjv01 <verma.rajshekhar1@gmail.com>
2025-12-02 23:06:47 +0530

    adding ui and formatting code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [prompt,setPrompt] = useState("");
   const [answer,setAnswer] = useState("");
   const [loading,setLoading] = useState(false);
+  const [prevQues,setPrevQues] = useState("");
   
   async function generateAnswer(){
 
@@ -13,6 +14,8 @@ function App() {
     }else{
       console.log("loading....");
       setLoading(true);
+      setPrevQues(prompt);
+      setPrompt("");
       const response = await axios({
         url:`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${
             import.meta.env.VITE_API_GENERATIVE_LANGUAGE_CLIENT
@@ -32,30 +35,42 @@ function App() {
 
   }
   return (
-    <div className='min-h-screen flex flex-col items-center bg-slate-600'>
+    <div className='min-h-screen flex flex-col items-center bg-black'>
       <h1 className='text-3xl p-3 m-3 text-blue-500'>Chat with Ai</h1>
-      <div className='flex flex-col border w-[900px] p-[70px] m-3 rounded-lg'>
+      <div className='flex flex-col border border-white shadow-xl w-[900px] p-[70px] m-3 rounded-lg'>
+        {
+          loading ? (
+            <p className='text-xl text-white'>Loading... free-tier services can be a bit slow.</p>
+          ) : (
+            <div>
+              {
+                prevQues && (
+                  <div className='flex flex-col border border-white rounded-xl p-6 m-3 mb-8'>
+                    <p className='bg-slate-50 p-3 m-3 rounded-lg font-bold'>{"> "}{prevQues}</p>
+                    <pre 
+                      className='text-lg text-blue-300  whitespace-pre-wrap wrap-break-words overflow-auto max-h-[500px]'
+                    >
+                        {answer}
+                    </pre>
+                  </div>
+                )
+              }
+            </div>
+          )
+        }
         <textarea 
           rows="10"
           cols="30"
           value={prompt}
           onChange={(e)=>setPrompt(e.target.value)}
-          style={{ padding:"12px"}}
           placeholder='Enter your prompt'
-          className='border rounded-2xl text-xl'
+          className='border p-3 m-3 text-white rounded-2xl text-xl'
         ></textarea>
         <button 
           onClick={generateAnswer}
-          className='text-2xl border rounded-lg m-4 p-4 hover:bg-slate-800/50 hover:shadow-2xl duration-300 cursor-pointer'
+          className='text-2xl border rounded-lg m-4 p-4 bg-white hover:bg-white/50 duration-300 cursor-pointer'
         >
           Generate Answer</button>
-        {
-          loading ? (
-            <p>loading..</p>
-          ) : (
-            <p>{answer}</p>
-          )
-        }
       </div>
     </div>
   )
